25d853806015bcb16d54504c7bca785f578686b8
NIKHIL DAS <70520690+nikhildas273@users.noreply.github.com>
2023-04-05 12:38:15 +0000

    second

---
+++ src/Components/Card/Card.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+    <div className='container'>
+        <div className="card">
+            <div className="face">
+                <div className="face2">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Card

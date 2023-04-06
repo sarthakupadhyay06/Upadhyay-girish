2a2f2eed4d44a528528d9e35e000b9539f940be8
NIKHIL DAS <70520690+nikhildas273@users.noreply.github.com>
2023-04-06 12:35:30 +0000

    service-1

--- src/Pages/Services/Services.jsx
+++ src/Pages/Services/Services.jsx
@@ -5,66 +5,32 @@ import Card from '../../Components/Card/Card'
 import './Services.css'
 
 export default function Services() {
-  const Services = [{
+  const Services = [
+  {
     id: 1,
-    tag: "Tax",
+    tag: "Tax Service",
     src: tax,
-    info: `Upadhyay Brother's provides domestic tax services to individuals, corporations, partnerships, trusts & estates, non-profit organizations.
-
-    Our goal is to assist our clients with innovative tax planning, strong tax advocacy, and effective domestic tax strategies.
-    
-    This includes the following:
-    
-        Comprehensive advice on tax matters & tax planning;
-    
-        Advising clients on tax-effective compensation structuring for their employees;
-    
-        Preparation, review, and filing of corporate and non-corporate return and e-returns and withholding tax compliances;
-    
-        Representation before Income Tax Authorities;
-    
-        Assistance in availing exemption certificate and approvals;
-    
-        Conducting tax compliance review;`
-  },{
-    id: 1,
+    info: `Upadhyay Girish & Company provides domestic tax services to individuals, corporations, partnerships, trusts & estates, non-profit organizations. Our goal is to assist our clients with innovative tax planning, strong tax advocacy, and effective domestic tax strategies.`
+  },
+  {
+    id: 2,
+    tag: "Income Tax",
+    src: tax,
+    info: ``
+  },
+  {
+    id: 3,
+    tag: "Gst Services",
+    src: gst,
+    info: `We provides end-to-end GST services from GST registration to GST return and compliance. You can easily create invoices, track expenses, manage inventory and file GST returns through our cloud-based GST software. Get in touch with our Experts for GST related advisory.`
+  },
+  {
+    id: 2,
     tag: "Tax plan",
     src: taxplan,
-    info: `Upadhyay Brother's provides domestic tax services to individuals, corporations, partnerships, trusts & estates, non-profit organizations.
-
-    Our goal is to assist our clients with innovative tax planning, strong tax advocacy, and effective domestic tax strategies.
-    
-    This includes the following:
-    
-        Comprehensive advice on tax matters & tax planning;
-    
-        Advising clients on tax-effective compensation structuring for their employees;
-    
-        Preparation, review, and filing of corporate and non-corporate return and e-returns and withholding tax compliances;
-    
-        Representation before Income Tax Authorities;
-    
-        Assistance in availing exemption certificate and approvals;
-    
-        Conducting tax compliance review;`
-  },{
-    id: 1,
-    tag: "Gst",
-    src: gst,
-    info: `GST was introduced in 2016 and has been a game changer. It has helped in the ease of doing business. From a multitude of taxes at the centre and state level, it has be simplified to a single tax. Upadhyay Brother's provides services in GST registration and migration, calculation of GST liabilities and credits, filing of GST returns, liaising with authorities in cases of tax demands, scrutiny or disputes for both GST and erstwhile indirect tax issues (service tax, VAT, excise, etc.), interpretation of GST Act and specific sections depending upon the clients’ industry, products, exporter/importer status etc.
-
-    Appropriate classification in terms of HSN and SAC codes and the applicable tax rates;
-    
-    Ascertain place of supply to determine relevant state for GST compliances and tax payments;
-    
-    Claim proper input tax credits where eligible and exclude or defer input tax credits not eligible as well as refunds or tax credits for exports
-    
-    Ascertain and ensure compliance with reverse charge on inward supplies where applicable;
-    
-    Structuring of contracts / transactions to optimize tax incidence;
-    
-     Conducting tax impact assessment studies. `
-  }]
+    info: ``
+  }
+]
   return (
   <div className="services">
     <div className="service-heading-stick">
@@ -78,7 +44,7 @@ export default function Services() {
       <div className="container service-container">
         {
           Services.map(service =>
-            <Card tag={service.tag} src={service.src} info={service.info}/>
+            <Card key={service.id} tag={service.tag} src={service.src} info={service.info}/>
           )
         }
       </div>

ad5fb150c68a1e1bce3678bb75b5f54c8d8e5698
sarthakupadhyay06 <71659349+sarthakupadhyay06@users.noreply.github.com>
2023-04-07 10:04:40 +0000

    team changes

--- src/Pages/Team/Team.jsx
+++ src/Pages/Team/Team.jsx
@@ -1,4 +1,5 @@
 import "./Team.css";
+import Aduit from '../../Asset/Audit.jpg'
 export default function Team() {
 return (
   <>
@@ -17,13 +18,76 @@ return (
     </div>
     <div className="team-detail">
       <div className="girish">
+      <img src={Aduit} alt="" />
+           <div className="team-info">
+              <h2>
+              CA. Girish Upadhyay
+              </h2>
+              <span>
+              (Founder & Managing Partner)
+              </span>
+              <span>
+              Founded this accounting firm with a simple principle: Our success is a direct result of each client’s success.
+              </span>
+              <span>
+              Contact No. 9958073472,8447476622 
+              </span>
+              <span>
+              Email Id :- cagirishupadhyay@gmail.com
+              </span>
+              <span className="space-email">
+               cagirishupadhyay@yahoo.com
+               </span>
+               <span className="space-email"> 
+               cagupadhyay@gmail.com
+              </span>
+           </div>
+          
 
       </div>
       <div className="chetan">
+      <img src={Aduit} alt="" />
+      <div className="team-info">
+              <h2>
+              CA. Chetan Upadhyay
+              </h2>
+              <span>
+              (Managing Partner)
+              </span>
+              <span>
+              As Managing Partner, it is my privilege to ensure that our firm continues to serve the needs of our clients and remains true to the principles that have sustained us for many decades. 
+              </span>
+              <span>
+              Contact No. 7503422525
+              </span>
+              <span>
+              Email Id :- cachetanupadhyay@gmail.com
+              </span>
 
+           </div>
+          
       </div>
       <div className="bhawna">
-        
+      <img src={Aduit} alt="" />
+        <div className="team-info">
+              <h2>
+              CS. Bhawna Gaur
+              </h2>
+              <span>
+              ( Associates Partner)
+              </span>
+              <span>
+              As Associates Partner, it is my privilege to ensure that our firm continues to serve the needs of our clients.
+              </span>
+              <span>
+              Contact No. 7011719573
+              </span>
+              <span>
+              Email Id :- csbhawna.gaur@gmail.com
+              </span>
+
+           </div>
+          
       </div>
     </div>
   </>
